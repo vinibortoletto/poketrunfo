82ca38195cf518903f355209e418049af87c1428
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-09-10 21:38:50 -0300

    feat: fetch pokemons functions

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,47 @@ import PreGame from './pages/PreGame/PreGame';
 // Styles
 import './helpers/styles/reset.css';
 import GlobalStyles from './helpers/styles/Global.style';
-import { getAllPokemons } from './api/pokeapi';
+import { fetchFirstGenPokemons, fetchPokemonInfo } from './api/pokeapi';
 
 export default class App extends Component {
-  componentDidMount() {
-    getAllPokemons();
+  state = {
+    deck: [],
+  };
+
+  async componentDidMount() {
+    this.getRandomDeck();
   }
+
+  getRandomPokemon = async () => {
+    const deck = await fetchFirstGenPokemons();
+    const randomNumber = Math.floor(Math.random() * 151);
+    const randomPokemon = deck[randomNumber];
+    return randomPokemon;
+  };
+
+  getRandomDeck = async () => {
+    const randomPokemon = await this.getRandomPokemon();
+    const randomPokemonInfo = await fetchPokemonInfo(randomPokemon.name);
+    const randomDeck = [randomPokemonInfo];
+
+    console.log(randomPokemonInfo);
+
+    for (let i = 0; randomDeck.length !== 20; i += 1) {
+      const newRandomPokemon = await this.getRandomPokemon();
+      const hasPokemon = randomDeck.some(({ name }) => name === newRandomPokemon.name);
+
+      if (!hasPokemon) {
+        const newRandomPokemonInfo = await fetchPokemonInfo(newRandomPokemon.name);
+        randomDeck.push(newRandomPokemonInfo);
+      }
+    }
+
+    this.setState({ deck: [...randomDeck] });
+  };
 
   render() {
     const { location: { pathname } } = this.props;
+    const { deck } = this.state;
 
     return (
       <>
@@ -30,7 +62,11 @@ export default class App extends Component {
 
         <main>
           <Route exact path="/" component={Welcome} />
-          <Route exact path="/pre-game" component={PreGame} />
+          <Route
+            exact
+            path="/pre-game"
+            render={() => <PreGame deck={deck} />}
+          />
         </main>
 
         <Footer />
--- src/api/pokeapi.js
+++ src/api/pokeapi.js
@@ -1,7 +1,14 @@
 const ENDPOINT = 'https://pokeapi.co/api/v2/pokemon/';
+const FIRST_GEN_POKEMONS = `${ENDPOINT}?limit=151`;
 
-export const getAllPokemons = async () => {
-  const response = await fetch(ENDPOINT);
+export const fetchFirstGenPokemons = async () => {
+  const response = await fetch(FIRST_GEN_POKEMONS);
   const data = await response.json();
-  console.log(data);
+  return data.results;
+};
+
+export const fetchPokemonInfo = async (pokemonName) => {
+  const response = await fetch(`${ENDPOINT}${pokemonName}`);
+  const data = await response.json();
+  return data;
 };
